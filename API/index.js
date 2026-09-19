d270d1297651bd81ceafde7deaedebbb986b9c4c
hugorocha-web <hjj78092@gmail.com>
2026-09-18 22:33:03 -0300

    first commit

---
+++ API/index.js
@@ -0,0 +1,86 @@
+import express, { response } from 'express'
+import mongoose from 'mongoose'
+import cors from 'cors'
+//importando primeiro o express e a ferramenta pra conectar 
+//com o banco de dados
+
+
+
+const port = 3000
+const app = express()
+
+//depois inicio o express colocando na variavel app
+
+mongoose.connect('mongodb+srv://roxshugo424_db_user:yUFleDtHuQiqiZhg@cluster0.6udxcla.mongodb.net/Usuarios?appName=Cluster0')
+.then(()=> console.log('banco conectado com sucesso'))
+.catch(()=> console.log('erro ao conectar o banco'))
+
+//isso cria um formato de arquivo pro banco
+const usuarioSchema = new mongoose.Schema({
+    nome: {type: String, require: true},
+    idade: {type: Number, require: true},
+    email: {type: String, require: true, unique:true}
+}, {timestamps: true}
+)
+
+//criei o usuario agora é mandar pra ele modelar
+const Usuario = mongoose.model('Usuario', usuarioSchema)
+
+//agora conectei o bando e adicionei o then 
+// mongodb+srv://roxshugo424_db_user:yUFleDtHuQiqiZhg@cluster0.6udxcla.mongodb.net/Usuarios?appName=Cluster0
+app.use(cors())
+app.use(express.json())
+//avisando que vou usar json
+
+
+
+//agora defino rotas
+//vamos usar os promises(promessa)
+app.get('/users', async (req, res) => {//mandar os usuarios
+    let usuariosBanco = await Usuario.find()
+    res.json(usuariosBanco)
+
+})
+app.post('/users', async (req, res) => {//cadastrar novos usuarios
+    try {
+        let novouser = req.body
+        let usuarioCriado = await Usuario.create(novouser)
+        res.json(usuarioCriado)
+    } 
+    catch (error) {
+        if (error.code === 11000) {
+            return res.status(409).json({
+                erro: 'Este e-mail já está cadastrado'
+            })
+        }
+        res.status(500).json({
+            erro: 'Erro interno do servidor'
+        })
+    }
+
+})
+app.delete('/users/:email', async (req, res) => {
+    const email = req.params.email
+
+    const usuario = await Usuario.findOneAndDelete({ email: email })
+
+    if (!Usuario) {
+        return res.status(404).json({
+            erro: 'Usuário não encontrado'
+        })
+    }
+
+    res.json({
+        mensagem: 'Usuário deletado'
+    })
+
+
+})
+
+
+
+// servidor precisa saber qual a porta esperar requisições
+app.listen(port, ()  => {
+    console.log('servidor rodando na porta 3000')
+})
+
---
+++ front-end/front.js
@@ -0,0 +1,130 @@
+const btn = document.querySelector('#btn')
+btn.addEventListener('click', enviar)
+let body = document.querySelector('article')
+let clonado = document.querySelector('.usuario')
+
+async function enviar(){
+    console.log('entrou')
+    let nome = document.querySelector('#idnome').value
+    if(nome === ''){
+        console.log('não tem nome')
+        return
+    }
+    let email = document.querySelector('#idemail').value.trim()
+    if(email === ''){
+        console.log('não tem email')
+        return
+    }
+
+    let idade = document.querySelector('#ididade').value
+    if(idade === ''){
+        console.log('não tem idade')
+        return
+    }
+
+    console.log(nome, email, idade)
+
+    let resposta = await fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            nome: nome,
+            idade: idade,
+            email: email
+        })
+    })
+    let dados = await resposta.json()
+    if (!resposta.ok) {
+
+        console.log('aq')
+        const popup = document.getElementById('popup');
+        popup.classList.add('active');
+        popup.querySelector('#tit').textContent = dados.erro
+        let btnclose = document.querySelector('.popup-close')
+        popup.addEventListener('click', (event) => {
+            console.log(popup)
+            if (event.target === popup) {
+                popup.classList.remove('active');
+            }
+            else if(event.target === btnclose){
+                popup.classList.remove('active');
+                
+            }
+        });
+    }
+    else{
+        coisas()
+    }
+
+
+
+
+}
+async function coisas() {
+    body.innerHTML=''
+    try {
+        
+
+        let dados = await fetch('http://localhost:3000/users', {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        }})
+
+
+        let json = await dados.json()
+
+
+        
+
+        for (let i = 0; i < json.length; i++){
+            let clone = clonado.cloneNode(true);
+            clone.querySelector('#btndelet').addEventListener('click', apagar)
+            clone.querySelector(".nome").textContent = "Nome: " + json[i].nome
+            clone.querySelector(".email").textContent = "Email: " + json[i].email
+            clone.querySelector(".idade").textContent = "Idade: " + json[i].idade
+            clone.style.display = 'flex'
+            body.appendChild(clone)
+
+        }
+
+
+
+
+
+
+    } 
+    catch (error) {
+        console.log(error)
+    }
+    
+}
+window.onload = function() {
+  coisas()
+};
+
+async function apagar(event){
+    try {
+        
+
+        let dados = await fetch(`http://localhost:3000/users/${event.target.previousElementSibling.children[1].textContent.slice(7)}`, {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json'
+        }})
+
+
+        coisas()
+
+
+
+
+
+
+    } 
+    catch (error) {
+        console.log(error)
+    }
+}
